f5662214d307a3928042737cf263ac7b131111ad
Christophe Charbonneau-Freeston <christophefreeston@googlemail.com>
2022-10-27 14:17:29 +0100

    Initial Commit

---
+++ LetterChain/LetterChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterChainApp.swift
+//  LetterChain
+//
+//  Created by Christophe on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LetterChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
